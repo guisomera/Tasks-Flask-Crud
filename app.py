cf28e60a12b42ca964c5fcdbff6be4a646b0a49b
guisomera <guisomera22@gmail.com>
2025-07-31 16:42:48 -0300

    Create endpoints to list all tasks and retrieve task by ID

--- app.py
+++ app.py
@@ -13,12 +13,30 @@ task_id_control = 1
 def create_task():
     global task_id_control
     data = request.get_json()                   #Pega tudo que o usuário (Postman) escreveu e mandou, transformando em dicionário
-    new_task = TaskManager(id= task_id_control, tittle=data.get("tittle"), description=data.get("description"))  #Data.get() pra pegar a info de dentro da chave tal, atribuindo aos elementos da minha classe
+    new_task = TaskManager(id= task_id_control, title=data.get("title"), description=data.get("description"))  #Data.get() pra pegar a info de dentro da chave tal, atribuindo aos elementos da minha classe
     task_id_control += 1
     tasks.append(new_task)
     print(tasks)
     return jsonify({"message": "Nova tarefa criada com sucesso!!!"})
 
+@app.route("/tasks", methods=['GET'])
+def get_tasks   ():
+    task_list = [task.to_dict() for task in tasks]              #Criamos outra lista pra dar saida, cada item em tasks vai ser adicionado em task_list pelo metodo to_dict()
+
+    output = {                                      #Criou uma variavel de saida 
+        "tasks": task_list,                         #Armazenou as listas 
+        "total_tasks": len(task_list)
+    }
+    print(task_list)
+    return jsonify(output)                          #Retornou a variável de saida em json
+
+@app.route("/tasks/<int:id>", methods=['GET'])
+def get_task(id):
+    for t in tasks:                                 #Percorrendo a lista de atividade
+        if t.id == id:                              #Pra ver se encontra o id q foi solicitado
+            return jsonify(t.to_dict())             #Retornando essa atividade 
+        
+    return jsonify({"Message": "Não foi possível encontrar a atividade"}), 404
 
 if __name__ == "__main__":      #Basicamente diz: "Se este arq  uivo esta sendo executado diretamente (e nao importado), rode o Flask"
     app.run(debug=True)         #Rodar o Flask e debug é utilizado para desenvolvedores
--- models/task.py
+++ models/task.py
@@ -1,14 +1,14 @@
 class TaskManager:
-    def __init__(self, id, tittle, description, completed=False):
+    def __init__(self, id, title, description, completed=False):
         self.id = id
-        self.tittle = tittle
+        self.title = title
         self.description = description
         self.completed = completed 
 
     def to_dict(self): 
         return {
+            "title": self.title,
             "id": self.id,
-            "tittle": self.tittle,
             "description": self.description,
             "completed": self.completed
         }
